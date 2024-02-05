43d3475d10b93e9cf91ce2ffa95822b46c04d31b
l3lack30 <119925244+l3lack30@users.noreply.github.com>
2024-02-05 21:30:23 +0700

    Sarayut Senarsa 6404101341

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -28,3 +28,64 @@ export default {
 </script>
 
 <style scoped></style>
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <form>
+      <input type="text" id="firstValue" v-model="firstValue" />
+      <input type="text" id="secondValue" v-model="secondValue" />
+      <input type="text" id="answerVulue" v-model="answerVulue" />
+      <button @click="addition(firstValue,secondValue)" >Addition</button>
+      <button @click="subtraction(firstValue,secondValue)" >Subtraction</button>
+      <button @click="multiplication(firstValue,secondValue)" >Multiplication</button>
+      <button @click="divide(firstValue,secondValue)" >Divide</button>
+      <input type="text" id="firstName" v-model="firstName" />
+      <input type="text" id="LastName" v-model="LastName" />
+      <input type="text" id="id" v-model="id" />
+
+  </form>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: 'Vue is awesome.',
+      firstValue:0,
+      secondValue:0,
+      answerVulue:0,
+      firstName:'Sarayut',
+      LastName:'Senarsa',
+      id:"6404101341"
+    }
+  },
+  methods: {
+    // https://jestjs.io/docs/using-matchers
+    // https://www.philbarresi.com/jest-test-guide/
+    addition(first, second) {
+      this.answerVulue =  Number(first) + Number(second)
+      return this.answerVulue;
+    },
+    subtraction(first, second ) {
+      this.answerVulue =  Number(first) - Number(second)
+      return this.answerVulue;
+    },
+    multiplication(first, second ) {
+      this.answerVulue =  Number(first) * Number(second)
+      return this.answerVulue;
+    },
+    divide(first, second) {
+      this.answerVulue =  Number(first) / Number(second)
+      return this.answerVulue;
+    },
+  },
+}
+
+
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
